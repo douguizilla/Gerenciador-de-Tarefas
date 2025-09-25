0cd939e1872ea7fdd46ba3ae893bdb38a8a2a4b4
Douglas Gomes <douglasgommez@gmail.com>
2025-09-25 17:20:41 -0300

    feat: add minimium deployment target as 17

---
+++ Gerenciador de Tarefas/View/TaskRowView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskRowView.swift
+//  Gerenciador de Tarefas
+//
+//  Created by Douglas Gomes on 24/09/25.
+//
+
+import SwiftUI
+
+struct TaskRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskRowView()
+}
