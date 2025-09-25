0274e89fa273f80ca6496bbc9ad32e6f44518013
Douglas Gomes <douglasgommez@gmail.com>
2025-09-24 22:17:05 -0300

    Initial Commit

---
+++ Gerenciador de Tarefas/Gerenciador_de_TarefasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gerenciador_de_TarefasApp.swift
+//  Gerenciador de Tarefas
+//
+//  Created by Douglas Gomes on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Gerenciador_de_TarefasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
