3b6e294f028b42c19b39b923857e14d3fd46c592
Douglas Gomes <douglasgommez@gmail.com>
2025-09-28 22:51:08 -0300

    feat: remove TaskView

--- Gerenciador de Tarefas/View/Home.swift
+++ Gerenciador de Tarefas/View/Home.swift
@@ -13,7 +13,7 @@ struct Home: View {
     @State private var currentWeekIndex: Int = 1
     @State private var createWeek: Bool = false
     
-    @State private var tasks: [Task] = sampleTasks.sorted(by: { $1.creationDate > $0.creationDate})
+    
     @State private var createNewTask: Bool = false
     
     @Namespace private var animation
@@ -24,7 +24,7 @@ struct Home: View {
             
             ScrollView(.vertical){
                 VStack{
-                    TaskView()
+                    TasksView(currentDate: $currentDate)
                 }
                 .hSpacing(.center)
                 .vSpacing(.center)
@@ -187,26 +187,6 @@ struct Home: View {
                     }
             }
         }
-    }
-    
-    @ViewBuilder
-    func TaskView() -> some View {
-        VStack(alignment: .leading, spacing: 35) {
-            ForEach($tasks){ $task in
-                TaskRowView(task: $task)
-                    .background(alignment: .leading) {
-                        if tasks.last?.id != task.id {
-                            Rectangle()
-                                .frame(width: 1)
-                                .offset(x: 8)
-                                .padding(.bottom, -35)
-                            
-                        }
-                    }
-            }
-        }
-        .padding([.vertical, .leading], 15)
-        .padding(.top, 15)
     }
     
     func paginateWeek(){
